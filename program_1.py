e63db712570b0bf7ec156a24fd3af3d1df9166c8
JosephUCRobotics <143100877+JosephUCRobotics@users.noreply.github.com>
2025-09-04 13:31:16 -0500

    program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,8 @@
+# Author: Joseph Kracht
+# Last Modified: 9/4/2025
+# Title: My Modified Hello World Program
+
 def hello_world():
-    # Update the message below to say "Hello World", 
-    # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    print("Hello World")
 
 hello_world()
